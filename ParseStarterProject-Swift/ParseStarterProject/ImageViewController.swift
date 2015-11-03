f91d8f5c0d81a8302fd2e0faa992f81aaaf7ceba
Miles Ranisavljevic <itstop3@gmail.com>
2015-11-02 16:12:35 -0800

    Add image view, utilize imagepickercontroller
    Set up imagepickercontroller to check for a camera and give the user the option of the photo source.  When a photo is chosen it gets saved on Parse

---
+++ ParseStarterProject-Swift/ParseStarterProject/ImageViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ImageViewController.swift
+//  InstaGlam
+//
+//  Created by Miles Ranisavljevic on 11/2/15.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+
+import Parse
+
+class ImageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePickerController = UIImagePickerController()
+    
+    
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func imagePickerButton(sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            let imagePickerAction = UIAlertController(title: "Choose Source", message: "Please choose your image source.", preferredStyle: .ActionSheet)
+            let cameraAction = UIAlertAction(title: "Camera", style: .Default) { (alert) -> Void in
+                self.imagePickerController.sourceType = .Camera
+                self.presentViewController(self.imagePickerController, animated: true, completion: nil)
+            }
+            let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .Default) { (alert) -> Void in
+                self.imagePickerController.sourceType = .PhotoLibrary
+                self.presentViewController(self.imagePickerController, animated: true, completion: nil)
+            }
+            imagePickerAction.addAction(cameraAction)
+            imagePickerAction.addAction(photoLibraryAction)
+            self.presentViewController(imagePickerAction, animated: true, completion: nil)
+        } else {
+            self.imagePickerController.sourceType = .PhotoLibrary
+            self.presentViewController(self.imagePickerController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePickerController.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        let parseImageClass = PFObject(className: "Image")
+        if let imageData = UIImageJPEGRepresentation(image, 1.0) {
+            let imageFile = PFFile(data: imageData)
+            parseImageClass["imageData"] = imageFile
+            parseImageClass.saveInBackgroundWithBlock { (saved, error) -> Void in
+                if saved {
+                    print("Successfully saved.")
+                } else {
+                    print("Error code: \(error?.code)")
+                }
+            }
+        }
+        self.imageView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
