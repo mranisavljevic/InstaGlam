f603ba677f3b6dded51ef02fbd09a9f1b2bf2366
Miles Ranisavljevic <itstop3@gmail.com>
2015-11-06 08:59:28 -0800

    Add palindrome test playground

---
+++ ParseStarterProject-Swift/ParseStarterProject/Palindrome.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func testForPalindrome(word: String) -> Bool {
+    let stringArray = Array(word.characters)
+    for i in 0..<stringArray.count {
+        if stringArray[i] != stringArray[stringArray.count - 1 - i] {
+            return false
+        }
+    }
+    return true
+}
+
+let wordToTest = "tacocat"
+
+let answer = testForPalindrome(wordToTest)
+
+let anotherWord = "palindrome"
+
+let anotherAnswer = testForPalindrome(anotherWord)
