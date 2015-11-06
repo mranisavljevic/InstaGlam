dc1b399abb0bd61d231bc84fb8ca89acce2ed7ae
Miles Ranisavljevic <itstop3@gmail.com>
2015-11-06 14:44:40 -0800

    Update Fibonacci playground to take doubles.

--- ParseStarterProject-Swift/ParseStarterProject/Fibonacci.playground/Contents.swift
+++ ParseStarterProject-Swift/ParseStarterProject/Fibonacci.playground/Contents.swift
@@ -2,11 +2,8 @@
 
 import UIKit
 
-//This would compute the first 100 numbers correctly, but trying an error is thrown if
-//the array goes above 92 items, maybe because the numbers go above maxInt?
-
-func fibonacci(first: Int, second: Int) -> [Int] {
-    var sequence = [Int]()
+func fibonacci(first: Double, second: Double) -> [Double] {
+    var sequence = [Double]()
     sequence.append(first)
     sequence.append(second)
     print(sequence.count)
@@ -18,24 +15,18 @@ func fibonacci(first: Int, second: Int) -> [Int] {
     return sequence
 }
 
-//fibonacci(1, second: 1)
+fibonacci(1, second: 1)
 
 
-//Here's an alternate version that returns only the last two numbers, which are all
-//that are needed to continue calculating the sequence.  Still fails after the same
-//number of iterations.  maxInt?
-
-func fibonacciAlternate(first: Int, second: Int) -> (Int, Int) {
-    var newInt = 0
+func fibonacciAlternate(first: Double, second: Double) -> (Double, Double) {
+    var newDouble = 0.0
     var sequenceSegment = (first, second)
     for _ in 1...98 {
-        if sequenceSegment.0 + sequenceSegment.1 < Int.max {
-            newInt = sequenceSegment.0 + sequenceSegment.1
+            newDouble = sequenceSegment.0 + sequenceSegment.1
             sequenceSegment.0 = sequenceSegment.1
-            sequenceSegment.1 = newInt
-        }
+            sequenceSegment.1 = newDouble
     }
     return sequenceSegment
 }
 
-//fibonacciAlternate(1, second: 1)
+fibonacciAlternate(1, second: 1)
